44e2489ca925923b2bd59737dce0090933c60a09
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-03-20 11:03:34 -0500

    log in and register added and working

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,35 @@
+import { Link, useNavigate } from "react-router-dom"
+
+export const NavBar = () => {
+    const navigate = useNavigate()
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/computers">PC Builds</Link>
+                <Link className="nav-link" to="/computers">My Builds</Link>
+                <Link className="nav-link" to="/favorites">My Favorites</Link>
+                <Link className="nav-link" to="/staffbuilds">Staff Curated Builds</Link>
+                <Link className="nav-link" to="/classes">Contact Us</Link>
+            </li>
+            {
+                (localStorage.getItem("velocity_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("velocity_token")
+                                navigate('/login')
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
---
+++ src/views/ApplicationViews.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../components/auth/Login"
+import { Register } from "../components/auth/Register"
+import { Authorized } from "./Authorized"
+
+export const ApplicationViews = () => {
+    return <>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<Authorized />}>
+
+            </Route>
+        </Routes>
+    </>
+}
