1b37cf9c08301627a9e1077decc34f929b5ae529
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-03-22 10:26:11 -0500

    fixed import on computer by customr component

--- src/components/computers/MyComputers.js
+++ src/components/computers/MyComputers.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getComputersByCustomer } from './api';
+import { getComputersByCustomer } from '../../managers/ComputerManager';
 
 export const ComputersByCustomer = () => {
   const [computers, setComputers] = useState([]);
@@ -25,19 +25,19 @@ export const ComputersByCustomer = () => {
     <div>
         {computers.map(computer => {return  (<div className="computer-card" key={computer.id}>
                 <div className="computerDetails">
-                <p className="pc-name">{computer.name}</p>
-                <p className="pc-description">{computer.description}</p>
-                <p className="pc-customer">{computer.customer.user.username}</p>
-                <p className="pc-psu">{computer.power_supply.title}</p>
-                <p className="pc-processor">{computer.processor.title}</p>
-                <p className="pc-gpu">{computer.gpu.title}</p>
-                <p className="pc-motherboard">{computer.motherboard.title}</p>
-                <p className="pc-ram">{computer.ram.title}</p>
-                <p className="pc-case">{computer.case.title}</p>
-                <p className="pc-cpu_cooler">{computer.cpu_cooler.title}</p>
-                <p className="pc-keyboard">{computer.keyboard.title}</p>
-                <p className="pc-mouse">{computer.mouse.title}</p>
-                <p className="pc-ssd">{computer.ssd.title}</p>
+                <p className="pc-name">Name: {computer.name}</p>
+                <p className="pc-description">Description: {computer.description}</p>
+                <p className="pc-customer">Created By: {computer.customer.user.username}</p>
+                <p className="pc-psu">PSU: {computer.power_supply.title}</p>
+                <p className="pc-processor">CPU: {computer.processor.title}</p>
+                <p className="pc-gpu">GPU: {computer.gpu.title}</p>
+                <p className="pc-motherboard">Motherboard: {computer.motherboard.title}</p>
+                <p className="pc-ram">RAM: {computer.ram.title}</p>
+                <p className="pc-case">Case: {computer.case.title}</p>
+                <p className="pc-cpu_cooler">CPU: {computer.cpu_cooler.title}</p>
+                <p className="pc-keyboard">Keyboard: {computer.keyboard.title}</p>
+                <p className="pc-mouse">Mouse: {computer.mouse.title}</p>
+                <p className="pc-ssd">SSD: {computer.ssd.title}</p>
                 <p className="pc-total-cost">Total Cost: ${computer.price}</p>
                 {/* { deleteButton(itemObj.id) }
                 <button class="btn btn-outline-light" onClick={ () => { navigate(`${ itemObj.id }/edit`)}}>Edit an Item</button> */}
