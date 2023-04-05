db9bf4248a31156df0313e0ffd5713886c5b0c24
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-04-04 23:16:52 -0500

    installed next.js to project

--- src/components/computers/NewComputer.js
+++ src/components/computers/NewComputer.js
@@ -118,8 +118,7 @@ export const CreateComputer = () => {
         }
         setTotalPrice(newTotalPrice.toFixed(2))
       }
-    
-    
+
 
     return (
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" >
